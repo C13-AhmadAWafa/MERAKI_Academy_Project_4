97045ed49b32ac5fac3a9605cd9100cff4d1d6f2
AhmadAWafa <ahmad.abdoullah.wafa@gmail.com~>
2025-01-04 22:12:29 +0300

    project4

--- backend/controllers/role.js
+++ backend/controllers/role.js
@@ -1,1 +1,24 @@
 //create role
+const roleModel=require('../models/roleSchema');
+const createRole=(req,res)=>{
+    const {role,permissions}=req.body
+    const newRole=new roleModel({
+        role,
+        permissions
+    })
+    newRole.save().then(
+        (result)=>{
+            res.status(201).json({
+                success:true,
+                message:`role created`
+                role:result,
+            });
+        }
+    ).catch((err)=>{
+        res.status(500).json({
+            success:false,
+            message:`server error`
+        });
+    })
+}
+module.exports={createRole}
