9f809d43e0ab1410bc670e9c06eb856ab9962b8a
AhmadAWafa <ahmad.abdoullah.wafa@gmail.com~>
2025-01-04 21:43:54 +0300

    project4

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -1,3 +1,27 @@
 //create cart , we create cart in the register
 //add to cart or delete from cart (update function)
 //
+const cart = require("../models/cartSchema");
+
+const createCart = (res, req) => {
+  const { user, products } = req.body;
+  const newCart = new cartModel({
+    user,
+    products,
+  });
+  newCart
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        successes: true,
+        message: "cart created",
+        result: true,
+      });
+    })
+    .catch((err) => {
+      res.status(409).json({
+        successes: false,
+        message: "items add to cart",
+      });
+    });
+};
--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -2,3 +2,33 @@
 //update category (by id )
 //delete category (by id)
 //get all category (.get)
+const categoryModel = require("../models/categorySchema");
+
+const registerCategory = (res, req) => {
+  const { Name, Img } = req.body;
+  const newCategory = new categoryModel({
+    Name,
+    Img,
+  });
+  newCategory.save().then((result) => {
+    res
+      .status(201)
+      .json({
+        successes: true,
+        message: "category created",
+        result: true,
+      })
+      .catch((err) => {
+        if (err.keyPattern) {
+          return res.status(409).json({
+            successes: false,
+            message: "category already created",
+          });
+        }
+      });
+  });
+};
+
+module.exports = {
+  registerCategory,
+};
